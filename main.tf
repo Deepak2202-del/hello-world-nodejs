ffb942c2679e5e98d3f63b60644df34916506519
Deepak2202-del <121391381+Deepak2202-del@users.noreply.github.com>
2024-05-17 16:32:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,61 +6,23 @@ resource "aws_ecs_cluster" "hello_world" {
   name = "hello-world-cluster"
 }
 
-resource "aws_ecs_task_definition" "hello_world" {
-  family                = "hello-world-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode          = "awsvpc"
-  cpu                    = 1024
-  memory               = 512
-  execution_role_arn   = "arn:aws:iam::419577552919:role/ecs-task-execution-role"
-  container_definitions = jsonencode([
-    {
-      name      = "hello-world"
-      image      = "${aws_ecr_repository.hello_world.repository_url}:latest"
-      cpu        = 10
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_ecs_service" "hello_world" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.hello_world.name
-  task_definition = aws_ecs_task_definition.hello_world.arn
-  desired_count   = 1
-  launch_type      = "FARGATE"
-  network_configuration {
-    awsvpc_configuration {
-      subnets          = ["subnet-0c2abb33b571382db"]
-      security_groups = [sg-0d9417ffe4833bd3e]
-      assign_public_ip = "ENABLED"
-    }
-  }
-}
-
 resource "aws_ecr_repository" "hello_world" {
-  name                 = "hello-world-repo"
+  name = "hello-world-repo"
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name        = "ecs-task-execution-role"
-  description = " Execution role for ECS tasks"
+  description = "Execution role for ECS tasks"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action    = "sts:AssumeRole"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
-        Effect = "Allow"
+        Effect    = "Allow"
       }
     ]
   })
@@ -80,5 +42,50 @@ resource "aws_security_group" "hello_world" {
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "${aws_ecr_repository.hello_world.repository_url}:latest"
+      cpu       = 10
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = ["subnet-0c2abb33b571382db"]
+    security_groups  = [aws_security_group.hello_world.id]
+    assign_public_ip = true
   }
 }
